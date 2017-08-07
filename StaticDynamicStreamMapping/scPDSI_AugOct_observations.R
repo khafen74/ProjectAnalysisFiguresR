7b249864b4d4a35c7b73299309bb35c221f326ab
Konrad Hafen <khafen74@gmail.com>
2017-08-07 12:11:55 -0700

    added misclassification models

--- StaticDynamicStreamMapping/scPDSI_AugOct_observations.R
+++ StaticDynamicStreamMapping/scPDSI_AugOct_observations.R
@@ -14,7 +14,9 @@ dat <- read.csv(fnDA)
 # Subset to months August to October --------------------------------------
 
 #First calculate some additional metrics
+#pdsi percentile at time of observation minus pdsi perctile at quad collection
 dat$perDiff <- dat$wyPer - dat$perctl
+#pdsi at time of observation minus pdsi at quad collection
 dat$pdsiDiff <- dat$wyPDSI - dat$quad_mean
 dat$misclass <- ifelse(((dat$FCODE==46006|dat$FCODE==55800)&dat$Category=="Dry")|
                          ((dat$FCODE==46003|dat$FCODE==46007)&dat$Category=="Wet"),1,0)
@@ -24,7 +26,8 @@ datAugOct <- subset(dat, dat$Month > 7 & dat$Month < 11)
 per <- subset(datAugOct, datAugOct$FCODE==46006 | datAugOct$FCODE==55800)
 
 
-# Test some models --------------------------------------------------------
+
+# Test models, probability of being classified wet ------------------------
 
 test.log <- glm(per$wet~per$perDiff, family="binomial")
 
@@ -38,7 +41,8 @@ sd.rm <- glmer(wet~sd+(1|REACHCODE), family=binomial, data=per)
 AICctab(quad.rm, obsPer.rm, obsPdsi.rm, pdsi.rm, per.rm, sd.rm, nobs=nrow(per))
 
 
-# Models with drainage area -----------------------------------------------
+
+# Probability wet, with drainage area -------------------------------------
 
 quad.rm <- glmer(wet~perctl+(1|DivDASqKM), family=binomial, data=per)
 obsPer.rm <- glmer(wet~wyPer+(1|DivDASqKM), family=binomial, data=per)
@@ -49,3 +53,17 @@ sd.rm <- glmer(wet~sd+(1|DivDASqKM), family=binomial, data=per)
 pdsisd <- glmer(wet~pdsiDiff+sd+(1|DivDASqKM), family=binomial, data=per)
 
 AICctab(quad.rm, obsPer.rm, obsPdsi.rm, pdsi.rm, per.rm, sd.rm, pdsisd, nobs=nrow(per))
+
+
+# Probability misclass, with drainage area --------------------------------
+
+quad.rm <- glmer(misclass~perctl+(1|DivDASqKM), family=binomial, data=datAugOct)
+obsPer.rm <- glmer(misclass~wyPer+(1|DivDASqKM), family=binomial, data=datAugOct)
+obsPdsi.rm <- glmer(misclass~wyPDSI+(1|DivDASqKM), family=binomial, data=datAugOct)
+quadPdsi.rm <- glmer(misclass~quad_mean+(1|DivDASqKM), family=binomial, data=datAugOct)
+pdsi.rm <- glmer(misclass~pdsiDiff+(1|DivDASqKM), family=binomial, data=datAugOct)
+per.rm <- glmer(misclass~perDiff+(1|DivDASqKM), family=binomial, data=datAugOct)
+sd.rm <- glmer(misclass~sd+(1|DivDASqKM), family=binomial, data=datAugOct)
+pdsisd <- glmer(misclass~pdsiDiff+sd+(1|DivDASqKM), family=binomial, data=datAugOct)
+
+AICctab(quadPdsi.rm, quad.rm, obsPer.rm, obsPdsi.rm, pdsi.rm, per.rm, sd.rm, pdsisd, nobs=nrow(per))
