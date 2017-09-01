fef822797cc79e340e0629276cd3827461a75874
KONRADSPC\khafe <khafen74@gmail.com>
2017-09-01 13:36:28 -0700

    identify years quads were collected in crb and save scpdsi tifffs

---
+++ StaticDynamicStreamMapping/quad_years_to_tiff.R
@@ -0,0 +1,31 @@
+
+# Do setup ----------------------------------------------------------------
+
+rm(list=ls())
+
+library(raster)
+
+fn <- "C:\\konrad\\USGS\\PROSPER_NHD\\data\\PDSI\\NETCDF\\crb_scpdsi_wymean_wgs84.tif"
+raswy <- brick(fn)
+
+fn2 <- "C:\\konrad\\USGS\\PROSPER_NHD\\data\\csv\\crb_quads_scpdsi.csv"
+crbquads <- read.csv(fn2)
+
+setwd("C:\\konrad\\USGS\\PROSPER_NHD\\data\\PDSI\\wyTIFF")
+
+
+# Get list of years -------------------------------------------------------
+
+years <- unique(crbquads$chck_year)
+years <- c(years, seq(2004,2016,1))
+years <- sort(years, decreasing = F)
+
+
+# Create indiividual tiff for each year -----------------------------------
+
+for (year in years)
+{
+  fntiff <- paste("scpdsi_", as.character(year),".tif", sep="")
+  band <- dim(raswy)[3] - (2016-year)
+  writeRaster(subset(raswy,band), fntiff, format="GTiff")
+}
